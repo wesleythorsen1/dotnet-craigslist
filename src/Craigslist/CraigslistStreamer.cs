0d98b44e81d73028e9eb1e51c59c2bcf8a2a86e7
Wesley Thorsen <wesley.thorsen@gmail.com>
2021-07-06 19:03:58 -0700

    Completed streamer

--- src/Craigslist/CraigslistStreamer.cs
+++ src/Craigslist/CraigslistStreamer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,15 +10,19 @@ namespace Craigslist
 {
     public interface ICraigslistStreamer
     {
-        IAsyncEnumerable<CraigslistListing> StreamListings(CraigslistSearchRequest request, TimeSpan refreshInterval, CancellationToken cancellationToken = default);
-        IAsyncEnumerable<CraigslistListingDetails> StreamListingDetails(CraigslistSearchRequest request, TimeSpan refreshInterval, CancellationToken cancellationToken = default);
+        IAsyncEnumerable<CraigslistListing> StreamListings(CraigslistSearchRequest request, CancellationToken cancellationToken = default);
+        IAsyncEnumerable<CraigslistListingDetails> StreamListingDetails(CraigslistSearchRequest request, CancellationToken cancellationToken = default);
     }
 
     public class CraigslistStreamer : ICraigslistStreamer
     {
+        private static readonly TimeSpan FIFTEEN_MINUTES = TimeSpan.FromMinutes(15);
+
         private readonly ICraigslistClient _client;
 
-        public CraigslistStreamer() : this (new CraigslistClient()) { }
+        public CraigslistStreamer() : this(new CraigslistClient()) { }
+
+        public CraigslistStreamer(HttpClient httpClient) : this(new CraigslistClient(httpClient)) { }
 
         internal CraigslistStreamer(ICraigslistClient client)
         {
@@ -26,47 +31,34 @@ namespace Craigslist
 
         public async IAsyncEnumerable<CraigslistListingDetails> StreamListingDetails(
             CraigslistSearchRequest request, 
-            TimeSpan refreshInterval, 
             [EnumeratorCancellation] CancellationToken cancellationToken = default)
         {
-            while (!cancellationToken.IsCancellationRequested)
+            await foreach(var listing in StreamListings(request, cancellationToken))
             {
-                var now = DateTime.UtcNow;
-
-                await foreach (var listing in GetListingsSince(request, now - refreshInterval, cancellationToken))
-                {
-                    var listingRequest = new CraigslistListingRequest(listing.ListingUrl);
-                    yield return await _client.GetListingAsync(listingRequest, cancellationToken);
-                }
-
-                var wait = refreshInterval - (DateTime.UtcNow - now);
-                if (wait > TimeSpan.Zero)
-                {
-                    await Task.Delay((int)wait.TotalMilliseconds, cancellationToken);
-                }
+                var listingRequest = new CraigslistListingRequest(listing.ListingUrl);
+                yield return await _client.GetListingAsync(listingRequest, cancellationToken);
             }
         }
 
         public async IAsyncEnumerable<CraigslistListing> StreamListings(
             CraigslistSearchRequest request, 
-            TimeSpan refreshInterval, 
             [EnumeratorCancellation] CancellationToken cancellationToken = default)
         {
+            var start = DateTime.UtcNow;
+            var lastListingDate = start - FIFTEEN_MINUTES;
+
             while (!cancellationToken.IsCancellationRequested)
             {
-                var now = DateTime.Now;
-                var lastListingDate = now - TimeSpan.FromMinutes(60);
-
                 await foreach (var listing in GetListingsSince(request, lastListingDate, cancellationToken))
                 {
-                    if (lastListingDate < listing.Date)
+                    if (lastListingDate < listing.Date.ToUniversalTime())
                     {
-                        lastListingDate = listing.Date;
+                        lastListingDate = listing.Date.ToUniversalTime();
                     }
                     yield return listing;
                 }
 
-                var wait = refreshInterval - (DateTime.Now - now);
+                var wait = FIFTEEN_MINUTES - (DateTime.UtcNow - start);
                 if (wait > TimeSpan.Zero)
                 {
                     await Task.Delay((int)wait.TotalMilliseconds, cancellationToken);
@@ -83,38 +75,12 @@ namespace Craigslist
             {
                 var searchResults = await _client.SearchAsync(request, cancellationToken);
 
-                var listings = searchResults.Listings.Reverse().Where(l => l.Date >= since);
+                var listings = searchResults.Listings
+                    .Where(l => l.Date.ToUniversalTime() >= since)
+                    .OrderBy(l => l.Date);
 
                 foreach (var listing in listings)
                 {
-                    yield return listing;
-                }
-
-                if (listings.Count() < searchResults.Listings.Count() || searchResults.Next == default)
-                {
-                    // no more results
-                    yield break;
-                }
-
-                request = new CraigslistSearchRequest(searchResults.Next);
-            }
-        }
-
-        private async IAsyncEnumerable<CraigslistListing> TakeListingsWhile(
-            CraigslistSearchRequest request, 
-            Func<CraigslistListing, bool> shouldContinue,
-            [EnumeratorCancellation] CancellationToken cancellationToken = default)
-        {
-            while (!cancellationToken.IsCancellationRequested)
-            {
-                var searchResults = await _client.SearchAsync(request, cancellationToken);
-
-                foreach (var listing in searchResults.Listings.Reverse())
-                {
-                    if (!shouldContinue(listing))
-                    {
-                        yield break;
-                    }
                     yield return listing;
                 }
 
